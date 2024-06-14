8ce669e21af9d79054587597f94ab8280471fa37
RealMangoRage <64402114+RealMangorage@users.noreply.github.com>
2024-06-14 00:18:00 -0700

    Minor Fixes

--- fabric/src/main/java/org/mangorage/tiab/fabric/FabricTiabMod.java
+++ fabric/src/main/java/org/mangorage/tiab/fabric/FabricTiabMod.java
@@ -1,9 +1,12 @@
 package org.mangorage.tiab.fabric;
 
+import net.fabricmc.api.EnvType;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.fabricmc.loader.api.FabricLoader;
 import org.mangorage.tiab.common.CommonTiabMod;
+import org.mangorage.tiab.common.client.renderer.TimeAcceleratorEntityRenderer;
 import org.mangorage.tiab.common.core.CommonRegistration;
 import org.mangorage.tiab.common.core.LoaderSide;
 
@@ -16,5 +19,7 @@ public class FabricTiabMod extends CommonTiabMod implements ModInitializer {
     @Override
     public void onInitialize() {
         CommonRegistration.init();
+        if (FabricLoader.getInstance().getEnvironmentType() == EnvType.CLIENT)
+            EntityRendererRegistry.register(CommonRegistration.ACCELERATOR_ENTITY.get(), TimeAcceleratorEntityRenderer::new);
     }
 }
